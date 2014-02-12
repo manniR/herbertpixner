dd07bd61e6717cb85f70bf91b46ae8c5f24cd003
manniR <manni@manfredraggl.com>
2014-02-12 17:13:55 +0100

    page menu added

--- public/content/themes/320-child/functions.php
+++ public/content/themes/320-child/functions.php
@@ -61,26 +61,22 @@ function get_top_parent_page_id() {
 
 // add active class
 add_filter('nav_menu_css_class', 'add_my_active_class', 10, 2 );
+add_filter( 'page_css_class', 'add_my_active_class', 10, 5 );
 
 function add_my_active_class($classes, $item) {
 
     if( in_array( 'current-menu-item', $classes ) ||
         in_array( 'current-menu-ancestor', $classes ) ||
         in_array( 'current-menu-parent', $classes ) ||
-        in_array( 'current_page_parent', $classes ) ||
-        in_array( 'current_page_item', $classes ) ||
-        in_array( 'current_page_ancestor', $classes )
+        in_array( 'current-page-parent', $classes ) ||
+        in_array( 'current-page-item', $classes ) ||
+        in_array( 'current-page-ancestor', $classes )
     ) {
 
-        $classes[] = "active2";
+        $classes[] = "active";
     }
 
     return $classes;
-
-        /*if( $item->menu_item_parent == 0 && in_array('current-menu-item', $classes) ) {
-            $classes[] = "active2";
-        }
-        return $classes;*/
 
 }
 
@@ -96,31 +92,109 @@ function add_my_active_class($classes, $item) {
 register_nav_menus(
     array(
         // location => description
-          'theme_location' => 'hp_side_menu'
+         'theme_location' => 'hp_side_menu'
     )
 );
-
 
 function hp_side_nav() {
     // display the wp3 menu if available
     wp_nav_menu(
         array(
             //'menu' => 'main_nav', /* menu name */
-            'menu_class' => 'nav navbar-nav',
-            'theme_location' => 'hp_side_menu', /* where in the theme it's assigned */
+            //'menu_class' => 'nav navbar-nav',
+            //'theme_location' => 'hp_side_menu', /* where in the theme it's assigned */
             'container' => 'false', /* container class */
-            'fallback_cb' => 'wp_bootstrap_main_nav_fallback', /* menu fallback */
+            //'fallback_cb' => 'wp_bootstrap_main_nav_fallback', /* menu fallback */
             // 'depth' => '2',  suppress lower levels for now
-            'walker' => new Bootstrap_walker()
+            'walker' => new MR_Child_Only_Walker(),
+            'depth' => 0
         )
     );
 }
 
 
 
-class hp_page_side_walker extends Walker_page {
+
+/**
+ * Class child only walker menu
+ * @see Walker
+ *
+ */
+
+class MR_Child_Only_Walker extends Walker_Nav_Menu {
+
+
+    // Don't start the top level
+    function start_lvl(&$output, $depth=0, $args=array()) {
+
+        //$output .='alsdkfj';
+        if( 0 == $depth )
+            return;
+        parent::start_lvl($output, $depth, $args);
+    }
+
+    // Don't end the top level
+    function end_lvl(&$output, $depth=0, $args=array()) {
+
+
+
+        //$output .='aa';
+        if( 0 == $depth )
+            return;
+        parent::end_lvl($output, $depth, $args);
+    }
+
+    // Don't print top-level elements
+    function start_el(&$output, $object, $depth=0, $args=array(), $current_object_id=0) {
+
+        //$output .='start el: title: '. $object->post_title .' ';
+
+
+        if( 0 == $depth )
+            return;
+
+        parent::start_el($output, $object, $depth = 0, $args, $current_object_id);
+    }
+
+    function end_el(&$output, $object, $depth=0, $args=array()) {
+
+        $output .=" end el <br/>";
+
+
+        if( 0 == $depth )
+            return;
+
+        parent::end_el($output, $object, $depth, $args);
+    }
+
+
+
+    // Only follow down one branch
+    function display_element( $object, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
+        // Check if element as a 'current element' class
+        $current_element_markers = array( 'current-menu-item', 'current-menu-parent', 'current-menu-ancestor' );
+
+       /* echo '<pre>';
+        var_dump($object);
+        echo '</pre>';*/
+
+
+        //var_dump($output);
+        //$current_class = array_intersect( $current_element_markers, $element->classes );
+
+        // If element has a 'current' class, it is an ancestor of the current element
+        $ancestor_of_current = !empty($current_class);
+
+        // If this is a top-level link and not the current, or ancestor of the current menu item - stop here.
+        if ( 0 == $depth)
+            return;
+
+         parent::display_element( $object, $children_elements, $max_depth, $depth, $args, $output );
+    }
+} // end Child only Walker
 
 
 
 
-}  //End Walker Class
+
+
--- public/content/themes/320-child/page.php
+++ public/content/themes/320-child/page.php
@@ -2,7 +2,14 @@
 
 			<div id="content" class="clearfix row">
 
+                <?php wp_nav_menu(array('theme_location'=>'hp_side_menu', 'walker'=> new MR_Child_Only_Walker(), 'depth'=>2)) //hp_side_nav(); //get_sidebar(); // sidebar 1 ?>
+
+
 				<div id="main" class="col-sm-8 clearfix" role="main">
+
+
+
+
                     <?php
 
                     /**
@@ -10,24 +17,28 @@
                      */
 
 
-                   /* // Call class:
-                    $My_Walker = new My_Custom_Walker();
+                    // Call class:
+                    /*$My_Walker = new Bootstrap_walker();
 
-                    /*$args = array(
-                        'walker'      => $My_Walker
+                    $args = array(
+                        //'walker'      => $My_Walker,
+                        'depth' =>4
+
                     );
 
                     wp_list_pages( $args );*/
 
 
-/*
-                    echo '<pre>';
+
+
+
+                    /*echo '<pre>';
                     var_dump($post->ID);
                     echo '</pre>';
 
-                    $parent = get_top_parent_page_id();
+                    $parent = get_top_parent_page_id();*/
 
-                    $args = array(
+                    /*$args = array(
                         'walker' => new My_Custom_Walker(),
                         'depth' => 1,
                         'child_of' => $parent
@@ -37,6 +48,21 @@
 
 
                     ?>
+
+
+                    <?php
+                    if($post->post_parent)
+                        $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+                    else
+                        $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+                    if ($children) { ?>
+                        <ul>
+                            <?php echo $children; ?>
+                        </ul>
+                    <?php } ?>
+
+
+
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
@@ -60,7 +86,7 @@
 
 					</article> <!-- end article -->
 
-					<?php comments_template('',true); ?>
+
 
 					<?php endwhile; ?>
 
@@ -81,7 +107,7 @@
 
 				</div> <!-- end #main -->
 
-				<?php get_sidebar(); // sidebar 1 ?>
+
 
 			</div> <!-- end #content -->
 
