b0e5b72918e937f516657edcba1c4b6ab5e196bc
manniR <manni@manfredraggl.com>
2014-02-14 15:48:08 +0100

    tourdates query2

--- public/content/themes/320-child/tourdateListing.php
+++ public/content/themes/320-child/tourdateListing.php
@@ -2,36 +2,54 @@
 /*
  Template Name: Tourdates Listing
 */
-
 ?>
 
 <?php get_header(); ?>
 
+
 <?php
+
 $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
 $today = date('Y-m-d');
-query_posts(array(
-    'post_type' => 'performance',
+$args= array(
+    'post_type' => 'tourdate',
     'posts_per_page' => 5,
-    'caller_get_posts' => 5,
+    'ignore_sticky_posts' => true,
     'paged' => $paged,
-    'meta_key' => 'order-date',
+    'meta_key' => 'datum',
     'orderby' => 'meta_value',
     'order' => 'ASC',
     'meta_query' => array(
         array(
-            'key' => 'order-date',
-            'meta-value' => $value,
+            'key' => 'datum',
+            'meta-value' => $today,
             'value' => $today,
             'compare' => '>=',
             'type' => 'CHAR'
         )
     )
-));
 
-/* WP LOOP */
+);
 
-
+$tourdates_query = new WP_Query($args);
 ?>
+<?php if($tourdates_query->have_posts()): ?>
+<!-- the loop -->
+<?php while( $tourdates_query->have_posts() ) : $tourdates_query->the_post(); ?>
+
+<!--TOURDATES LISTING-->
+<h2><?php the_title() ?></h2>
+
+
+<?php endwhile ?>
+<!-- end of the loop -->
+
+<!-- pagination here -->
+
+<?php wp_reset_postdata(); ?>
+
+<?php else: ?>
+<p> <?php _e( 'Sorry, no tourdates where found'); ?></p>
+<?php endif; ?>
 
 
